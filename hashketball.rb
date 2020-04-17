a2c4cbf1eb653e3fd8907043114f533c38c706fa
Jesse Langley <jlangleyiii@aol.com>
2020-04-17 02:13:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,7 @@
 def game_hash
   game_hash =
  
-    {:home=>{:team_name=>"Brooklyn Nets", :colors=>["Black", "White"], :players=>[{:player_name => "Alan Anderson", :number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1}, {:player_name => "", :number => , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => }, {:player_name => "", :number => , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => }, {:player_name => "", :number => , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => }, {:player_name => "", :number => , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => }]},
+    {:home=>{:team_name=>"Brooklyn Nets", :colors=>["Black", "White"], :players=>[{:player_name => "Alan Anderson", :number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1}, {:player_name => "", :number => , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => }, {:player_name => "", :number => , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => }]},
       
       :away=>{:team_name=>"Charlotte Hornets", :colors=>["Turquoise", "Purple"], :players=>[{:player_name => "Jeff Adrien", :number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1}]}
     }
