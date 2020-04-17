cf6d098b7161899a40409c0977c4334cd9de72c3
Jesse Langley <jlangleyiii@aol.com>
2020-04-17 02:11:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,12 @@
-# Write your code here!
+def game_hash
+  game_hash =
+ 
+    {:home=>{:team_name=>"Brooklyn Nets", :colors=>["Black", "White"], :players=>[{:player_name => "Alan Anderson", :number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1}, {:player_name => "Alan Anderson", :number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1}]},
+      
+      :away=>{:team_name=>"Charlotte Hornets", :colors=>["Turquoise", "Purple"], :players=>[{:player_name => "Jeff Adrien", :number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1}]}
+    }
+    
+    
+
+  
+end
